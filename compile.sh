89511f761e2f8699ecaa7e2a718afa7fd67302ec
root <root@Zaks-PC>
2024-02-03 13:29:40 +0000

    removed #include<editline/history.h>

--- compile.sh
+++ compile.sh
@@ -2,31 +2,33 @@
 
 echo -e '\e[33m!--- You will need to have the cc or gcc compiler installed before running this script! ---!\e[0m'
 
-if [ `source /etc/os-release -eq "fedora"` ]
+source /etc/os-release
+
+if [ "$ID" == "fedora" ]
                 then
-		if [ dnf list installed | grep libedit-dev ]
-		then
-		cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
-		
-		else
+                if dnf list installed | grep libedit-dev
+                then
+                cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+
+                else
                 echo -e '\e[33mInstalling libedit-dev...\e[0m'
                 su -c "yum install libedit-dev*"
                 cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
-		fi
+                fi
 
 else
-	if ! dpkg -s libedit-dev
-       	then
-	echo -e '\e[33mInstalling libedit-dev...\e[0m'
+        if ! dpkg -s libedit-dev
+        then
+        echo -e '\e[33mInstalling libedit-dev...\e[0m'
         sudo apt install libedit-dev
-	fi
-                
-	cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+        fi
+
+        cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
 fi
 
-if [ $? -eq 1 ]
-	then
-	echo -e '\e[31mPlease ensure the cc or gcc compiler is installed before running this script\e[0m'
+if [ $? -eq 0 ]
+        then
+        echo -e '\e[32mCompilation successful!\e[0m'
 else
-	echo -e '\e[32mCompilation successful!\e[0m'
+        echo -e '\e[31mPlease ensure the cc or gcc compiler is installed before running this script\ndebian: sudo apt install build-essential\nfedora: su -c "yum groupinstall development-tools"\e[0m'
 fi
--- loremipsum.c
+++ loremipsum.c
@@ -18,7 +18,6 @@ void add_history(char* unused) {}
 
 #else
 #include <editline/readline.h>
-#include <editline/history.h>
 #endif
 
 enum
