f80b188fd52ca3671fdf9d255097c7d752234187
root <root@Zaks-PC>
2024-02-03 12:35:07 +0000

    adding errors and fedora support

--- compile.sh
+++ compile.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+echo -e '\e[33m!--- You will need to have the cc or gcc compiler installed! ---!\e[0m'
+
+if [ `dpkg -s libedit-dev` ]
+	then
+	cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+fi
+
+	if [ `source /etc/os-release -eq "fedora"` ]
+		then
+		echo -e '\e[33mInstalling libedit-dev...\e[0m'
+		su -c "yum install libedit-dev*"
+		cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+
+	else
+		echo -e '\e[33mInstalling libedit-dev...\e[0m'
+		sudo apt install libedit-dev
+		cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+	fi
+
+if [ $? -eq 1 ]
+	then
+	echo -e '\e[31mPlease ensure the cc or gcc compiler is installed before running this script\e[0m'
+fi
