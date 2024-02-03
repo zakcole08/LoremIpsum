292471936744e46cce70afce55f99ceb5fcac895
root <root@Zaks-PC>
2024-02-03 13:01:33 +0000

    fixed bugs for compilation script

--- compile.sh
+++ compile.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-echo -e '\e[33m!--- You will need to have the cc or gcc compiler installed! ---!\e[0m'
+echo -e '\e[33m!--- You will need to have the cc or gcc compiler installed before running this script! ---!\e[0m'
 
-if [ `dpkg -s libedit-dev` ]
-	then
+if [ `source /etc/os-release -eq "fedora"` ]
+                then
+		if [ dnf list installed | grep libedit-dev ]
+		then
+		cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+		
+		else
+                echo -e '\e[33mInstalling libedit-dev...\e[0m'
+                su -c "yum install libedit-dev*"
+                cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
+		fi
+
+else
+	if ! dpkg -s libedit-dev
+       	then
+	echo -e '\e[33mInstalling libedit-dev...\e[0m'
+        sudo apt install libedit-dev
+	fi
+                
 	cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
 fi
-
-	if [ `source /etc/os-release -eq "fedora"` ]
-		then
-		echo -e '\e[33mInstalling libedit-dev...\e[0m'
-		su -c "yum install libedit-dev*"
-		cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
-
-	else
-		echo -e '\e[33mInstalling libedit-dev...\e[0m'
-		sudo apt install libedit-dev
-		cc -std=c99 -Wall loremipsum.c mpc/mpc.c -ledit -lm -o loremipsum
-	fi
 
 if [ $? -eq 1 ]
 	then
 	echo -e '\e[31mPlease ensure the cc or gcc compiler is installed before running this script\e[0m'
+else
+	echo -e '\e[32mCompilation successful!\e[0m'
 fi
